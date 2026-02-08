4d59e9d755ee67811e3a34ba5f83e3f39d098cae
algodoncin <179851883+algodondev@users.noreply.github.com>
2026-02-07 22:05:07 -0600

    fix fetching

--- src/hooks/useFetchMovies.js
+++ src/hooks/useFetchMovies.js
@@ -34,7 +34,7 @@ export function useFetchMovies(query) {
         setError(null); // Reinicia errores previos
         // Petición a la API de OMDb con la clave de acceso y la consulta
         const response = await fetch(
-          `http://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`
+          `https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`
         );
         // Verifica si la respuesta HTTP es correcta
         if (!response.ok)
